5b09f4ec2b736bf0a0ea6da68af42cf9466cf6f6
Zim Kalinowski <zim.kalinowski@zoho.com>
2022-09-02 09:11:47 +0200

    just test pr

--- src/pytest_hello_world.py
+++ src/pytest_hello_world.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
-# SPDX-License-Identifier: CC0-1.0
+# SPDX-License-Identifier: CC0-1.0 
 
 from typing import Callable
 
@@ -7,15 +7,7 @@ import pytest
 from pytest_embedded_idf.dut import IdfDut
 from pytest_embedded_qemu.dut import QemuDut
 
-
-@pytest.mark.supported_targets
-@pytest.mark.generic
-def test_hello_world(dut: IdfDut, log_minimum_free_heap_size: Callable[..., None]) -> None:
-    dut.expect('Hello world!')
-    log_minimum_free_heap_size()
-
-
-@pytest.mark.esp32  # we only support qemu on esp32 for now
+@pytest.mark.esp32
 @pytest.mark.host_test
 @pytest.mark.qemu
 def test_hello_world_host(dut: QemuDut) -> None:
